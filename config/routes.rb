a9fbe06c52da855b2b13d8a0ebeb32d19df3f260
letizia obiglio <letizia.obiglio@gmail.com>
2019-08-05 14:15:22 +0200

    Resolved ecercice

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
